3279657e04fcf9998a1a97af5c5993548141d703
Metul <metulprabhu@gmail.com>
2026-03-28 11:25:44 +0530

    update artwork css

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// Change the base to match your GitHub repo name: '/your-repo-name/'
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-portfolio/', // <-- Change this to your GitHub repo name
+  base: '/personal-portfolio/',
 })
